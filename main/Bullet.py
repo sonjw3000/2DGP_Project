dec0d402cd1158d85fa28c5df6e2efa054824b51
Jungwon Desktop <sonjw3000@gmail.com>
2021-10-21 02:50:41 +0900

    Bullet Added

--- main/Bullet.py
+++ main/Bullet.py
@@ -1,28 +1,50 @@
 from pico2d import *
 
+BULLET_SPEED_X = 4
+BULLET_SPEED_Y = 3
+
+BULLET_SIZE = 20
+
 
 class Bullet:
 	__image = None
 
 	def __init__(self, x, y):
 		# It can active until x go + 500
-		self.__max_lifespan = 500
+		self.__max_lifespan = 150
 		self.__cur_life = 0
 		self.__x, self.__y = x, y
+
+		self.__y_Speed = -BULLET_SPEED_Y
+
 		self.__frame = 0
 
-	def update(self):
+	def update(self, y_col=False):
 		# Move here
-		self.__frame = (self.__frame + 1) % 40
-		self.__x += 1
+		self.__frame = (self.__frame + 2) % 40
 
-		return self.__cur_life >= self.__max_lifespan
+		self.__cur_life += 1
+
+		if y_col:
+			self.__y_Speed *= -1
+		if self.__y_Speed <= -BULLET_SPEED_Y:
+			self.__y_Speed = -BULLET_SPEED_Y
+
+		self.__x += BULLET_SPEED_X
+		self.__y += self.__y_Speed
+		self.__y_Speed -= 0.2
+
+		# return true when it is alive
+		return self.__cur_life < self.__max_lifespan
+
+	def get_position(self):
+		return (self.__x - BULLET_SIZE / 2,
+				self.__y - BULLET_SIZE / 2,
+				self.__x + BULLET_SIZE / 2,
+				self.__y + BULLET_SIZE / 2)
 
 	def draw(self):
-		self.__image.clip_draw(9 * (self.__frame // 10),  0, 9, 9, self.__x, self.__y, 20, 20)
-		pass
-
-	def test_fucn(self):
+		self.__image.clip_draw(9 * (self.__frame // 10), 0, 9, 9, self.__x, self.__y, BULLET_SIZE, BULLET_SIZE)
 		pass
 
 	@classmethod
