f684398412ae7720357cbfc3d285666103838ba1
Jungwon Desktop <sonjw3000@gmail.com>
2021-11-16 01:45:20 +0900

    Update GameWorld.py

--- main/GameWorld.py
+++ main/GameWorld.py
@@ -1,11 +1,19 @@
 #
 
-class GameObject:
-	objects = [[], []]
+# layer 0 : tiles
+# layer 1 : coins, items
+# layer 2 : monster
+# layer 3 : player
+# layer 4 : ui??
 
+class GameObject:
+	objects = [[], [], [], [], []]
+
+	@classmethod
 	def add_object(obj, layer):
 		objects[layer].append(obj)
 
+	@classmethod
 	def remove_object(o):
 		for i in range(len(objects)):
 			if o in objects[i]:
@@ -13,12 +21,14 @@ class GameObject:
 				del o
 				break
 
+	@classmethod
 	def clear():
 		for o in all_objects():
 			del o
 		for l in objects:
 			l.clear()
 
+	@classmethod
 	def all_objects():
 		for i in range(len(objects)):
 			for o in objects[i]:
