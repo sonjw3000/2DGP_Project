75b8d2251ceb686bae7370af93a067d8ec29de5f
Jungwon Desktop <sonjw3000@gmail.com>
2021-12-04 23:49:39 +0900

    now can load test map from files

--- main/GameWorld.py
+++ main/GameWorld.py
@@ -1,6 +1,7 @@
 #
+import os
 import pickle
-
+import server
 # layer 0 : tiles
 # layer 1 : coins, items
 # layer 2 : monster
@@ -52,7 +53,7 @@ def save():
 	# with open('game.sav', 'wb') as f:
 	# 	pickle.dump(objects, f)
 	with open("tiles.sav", "wb") as f:
-		pickle.dump(objects[0], f)
+		pickle.dump(server.tiles, f)
 
 	with open("monsters.sav", "wb") as f:
 		pickle.dump(objects[2], f)
@@ -63,5 +64,23 @@ def save():
 
 def load():
 	global objects
-	with open('game.sav', 'rb') as f:
-		objects = pickle.load(f)
+
+	clear()
+	# move to save directory
+
+	# load tiles
+	with open("tiles.sav", "rb") as f:
+		tiles = pickle.load(f)
+		for tile in tiles:
+			add_objects(tile, 0)
+		server.tiles = tiles
+
+	with open("monsters.sav", "rb") as f:
+		monster = pickle.load(f)
+		objects[2] = monster
+		server.monsters = monster
+
+	with open("player.sav", "rb") as f:
+		player = pickle.load(f)
+		objects[3] = player
+		server.gamePlayer = player
--- main/SuperMario.py
+++ main/SuperMario.py
@@ -1,8 +1,9 @@
 from pico2d import *
 import game_framework
 
+import world_build_state
 import main_state
 
 open_canvas(game_framework.w, game_framework.h)
-game_framework.run(main_state)
+game_framework.run(world_build_state)
 close_canvas()
